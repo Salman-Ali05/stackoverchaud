dc795b791aa8024f127d6378bf11f9cd65a47ace
Salman-Ali05 <sallukhan0805@gmail.com>
2025-07-02 12:34:29 +0200

    create user + archi redone

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,8 @@
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-
-const authRoute = require('./routes/auth.route');
-
 const app = express();
+const userRoutes = require('./routes/user.route');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev')); // simple log console
-app.use(cors());
-
-app.use('/api/auth', authRoute);
-
-app.get('/', (req, res) => {
-    res.status(200).send({
-        status: "success",
-        data: {
-            message: "API working fine"
-        }
-    });
-});
-
-// Middleware global d'erreur
-app.use((err, req, res, next) => {
-    res.status(err.statusCode || 500).send({
-        status: "error",
-        message: err.message
-    });
-});
+app.use('/api/users', userRoutes);
 
 module.exports = app;
