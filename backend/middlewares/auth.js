f78850f37aeb3f31c81f8f44604b8904345ea109
Salman-Ali05 <sallukhan0805@gmail.com>
2025-07-02 14:29:10 +0200

    role admin logic (settle on delete user for now)

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,22 +1,19 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET_KEY } = require('../utils/secrets');
 
-const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+module.exports = function verifyToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader?.split(' ')[1];
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ status: 'error', message: 'Token required' });
-    }
+  if (!token) {
+    return res.status(401).json({ status: 'error', message: 'Token required' });
+  }
 
-    const token = authHeader.split(' ')[1];
-
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET_KEY);
-        req.user = decoded; // contient id et role
-        next();
-    } catch (err) {
-        res.status(401).json({ status: 'error', message: 'Invalid or expired token' });
-    }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET_KEY);
+    req.user = decoded; // <-- Ici on attache le user
+    next();
+  } catch (err) {
+    return res.status(403).json({ status: 'error', message: 'Invalid token' });
+  }
 };
-
-module.exports = verifyToken;
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user.controller');
 const verifyToken = require('../middlewares/auth');
+const authorizeRoles = require('../middlewares/roles');
 
 router.post('/login', userController.login);
+router.post('/', userController.createUser);
+
 router.use(verifyToken);
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.delete('/:id', authorizeRoles("admin"), userController.deleteUser);
 
 module.exports = router;
