fd76942b34aace6aeb05ca68bc7bde9f9eaff2f7
Ali <sallukhan0805@gmail.com>
2025-07-06 21:20:37 +0200

    fix back api

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,4 @@
 const app = require('./app');
-const connectDB = require('./config/db.config');
 const serverless = require('serverless-http');
 
-connectDB();
-
-module.exports = app;
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
